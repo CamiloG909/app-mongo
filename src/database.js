eacb6de619c83214576c5c24130e543409b1fb34
Camilo Galindo R <cagalindo35@misena.edu.co>
2022-06-05 16:44:48 -0500

    new image login

--- src/database.js
+++ src/database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DB } = process.env;
 
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DB}`;
+const MONGODB_URI =
+	process.env.NOTES_APP_MONGODB_URI ||
+	`mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DB}`;
 
 mongoose
 	.connect(MONGODB_URI, {
